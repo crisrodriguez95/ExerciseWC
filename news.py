4323b0f4d7f4e5589ad5c758949bcb3d3566eb4c
crisrodriguez95 <62897412+crisrodriguez95@users.noreply.github.com>
2022-05-03 22:33:43 -0500

    solving errors
    solving errors

--- news.py
+++ news.py
@@ -5,8 +5,7 @@ import numpy as np
 from operator import itemgetter
 import operator
 
-#with scrapy i can extract the data from the url given html
-class News(scrapy.Spider):
+class News(scrapy.Spider):    
     name = 'news'
     start_urls = ['https://news.ycombinator.com/']
 
@@ -31,37 +30,39 @@ class News(scrapy.Spider):
                score = int(text[0: len(text) - len('score') - 1])              
             else:
                score = 0
-            text_a = entrie.css('.subtext').xpath('a/text()').getall()[-1]            
-            if 'comments' in text_a:
-               comments = int(text_a[0:len(text_a) - len('comments')-1])
+            text_detail = entrie.css('.subtext').xpath('a/text()').getall()[-1]            
+            if 'comments' in text_detail:
+               comments = int(text_detail[0:len(text_detail) - len('comments')-1])
             else:
                comments = 0            
           else: 
             i = 0            
             np.array_result.append([number, title, len(re.findall(r'\w+', title)), score, comments])
 
-        arr = np.array_result
+        array = np.array_result
+
+        
         
         #Showing the results
         print("==================================WEBCRAWLER================================== ")
         
         print("**The first 30 news** ")
-        for first30 in arr:
-            print(first30, "\n")
+        for entries30 in array:
+            print(entries30, "\n")
 
         print("==================================FILTERS================================== ")        
         print ("\n ** The entries with more than five words in the title and ordered by comments ** \n")
         
         #•	Filter all previous entries with more than five words in the title ordered by the number of comments first.
         
-        firstfilter =  sorted(filter(lambda a: a[2] > 5, arr), key=itemgetter(4), reverse= True)
-        for filterone in firstfilter:
-            print(filterone, "\n")
+        filterByComments =  sorted(filter(lambda a: a[2] > 5, array), key=itemgetter(4), reverse= True)
+        for byComment in filterByComments:
+            print(byComment, "\n")
 
         print ("\n ** The entries with less or equal to five words in the title and ordered by points ** \n")  
 
         #•	Filter all previous entries with less than or equal to five words in the title ordered by points.
         
-        secondfilter =  sorted(filter(lambda a: a[2] <= 5, arr), key = operator.itemgetter(3), reverse = True)
-        for filtertwo in secondfilter:
-            print(filtertwo, "\n")
+        filterByScore =  sorted(filter(lambda a: a[2] <= 5, array), key = operator.itemgetter(3), reverse = True)
+        for byScore in filterByScore:
+            print(byScore, "\n")
